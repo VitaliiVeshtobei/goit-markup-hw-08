53d04e1ea3ce1299832631157660d79c8f7853e6
VitaliiVeshtobei <veshtobey@gmail.com>
2022-06-04 15:18:38 +0300

    hmw mob menu

---
+++ js/menu-mob.js
@@ -0,0 +1,20 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-mobile]');
+  const mobilMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobilMenuRef.classList.toggle('is-open');
+  });
+})();
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-hidden]');
+  const mobilMenuRef = document.querySelector('body');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = mobilMenuRef.classList.toggle('menu-open');
+  });
+})();
