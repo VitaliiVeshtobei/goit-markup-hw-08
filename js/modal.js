00ffd10e6776db1b58f999a1909120aa012f451d
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-11-09 11:09:21 +0200

    fix modal scroll

--- js/modal.js
+++ js/modal.js
@@ -10,5 +10,18 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+
+    console.log(refs.openModalBtn.classList.contains('is-hidden'));
+
+    if (!refs.modal.classList.contains('is-hidden')) {
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${window.scrollY}px`;
+      document.body.style.width = `-webkit-fill-available`;
+      return;
+    }
+    const scrollY = document.body.style.top;
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
   }
 })();
